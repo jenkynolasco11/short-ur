48c38d4ecaed1acf3144f427d8ceb5dff86fa0f9
Jenky W. Nolasco <jenky.nolasco@gmail.com>
2016-10-20 09:54:35 -0300

    fixed bugs with database

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT;
 
+// app.use('/static',express.static(__direname));
+app.set('view engine', 'pug');
+app.set('views', './');
+
 
 var db = mongoose.connect('mongodb://bumbaneitor:abcde12345@ds061676.mlab.com:61676/short-url').connection;
 require('./schemas');
@@ -18,10 +22,9 @@ db.on('error',function(error){
 
 app.param('url',function(req,res,next,url){
   // http regex : /https?:\/\/[\w.-]+\:?\d{0,4}/
-  req.params.tempurl = url;
+  req.params['tempurl'] = url;
   ShortURL.findOne({"url":url},function(err,url){
     if (err) next(err);   // Send the error to be handled
-
     if(url){
       req.params.tinyurl = {
       'original_url' : url.url,
@@ -29,42 +32,47 @@ app.param('url',function(req,res,next,url){
       };
       next();
     }
+    else {
+      var checkIfExists = function(retries){
+        var retries = retries || 0;
+        var randomNumber = +(Math.random() * 10000).toFixed(0);
+        // console.log('executing the demonical function...');
+        // The demonical function!!!
+        (function(num, callback, retries){
+          console.log('debug2');
+          // console.log('Here I am... Checking for your ID...');
+          ShortURL.findOne({'id':num},function(err,res){
+            if(err) next(err);
+            if(res) {
+              if(retries > 10) {
+                console.log('ERROR: The max of tries trying to find a unique ID was reached');
+                next('ERROR: The max of tries trying to find a unique ID was reached');
+              }
+              // console.log('Here I am... Calling myself back again...');
 
-    var checkIfExists = function(){
+              callback(++retries);
+            }
+            else {
+              // console.log('Here I am! getting you a new ID');
+              var tinyurl = {
+                "id" : num,
+                "url" : req.params['tempurl'],
+                "short url" : req.protocol + '://' + req.get('host') + '/' + num
+              };
 
-    var randomNumber = +(Math.random() * 10000).toFixed(0);
-      // console.log('executing the demonical function...');
-      // The demonical function!!!
-      (function(num, callback){
+              var newshorturl = new ShortURL(tinyurl);
+              newshorturl.save(function(err){
+                if (err) next(err);
+                req.params['tinyurl'] = tinyurl;
+                next();
+              });
+            }
+          });
+        })(randomNumber, checkIfExists);
+      };
 
-        // console.log('Here I am... Checking for your ID...');
-        ShortURL.findOne({'id':num},function(err,res){
-          if(err) next(err);
-          if(res) {
-            // console.log('Here I am... Calling myself back again...');
-            callback();
-          }
-          else {
-            // console.log('Here I am! getting you a new ID');
-            var tinyurl = {
-              "id" : num,
-              "url" : req.params.tempurl,
-              "short url" : req.protocol + '://' + req.get('host') + '/' + num
-            };
-
-            var newshorturl = new ShortURL(tinyurl);
-            newshorturl.save(function(err){
-              if (err) next(err);
-              req.params.tinyurl = tinyurl;
-              next();
-            });
-          }
-        });
-      })(randomNumber, checkIfExists);
-    };
-
-    checkIfExists();
-
+      checkIfExists(0);
+    }
   });
 });
 
@@ -72,7 +80,7 @@ app.param('tinyurl',function(req,res,next,tinyurl){
   ShortURL.findOne({'id': tinyurl },function(err,url){
     if (err) next(err);     // Send the error to be handled
 
-    req.params.redirecturl = url || {};
+    req.params['redirecturl'] = url || {};
     next();
   });
 });
@@ -85,6 +93,12 @@ app.get('/new/:url(*)',function(req,res){
 app.get('/:tinyurl',function(req,res){
   if(req.params.redirecturl.url) res.redirect(req.params.redirecturl.url);
   else res.end('');
+});
+
+app.get('/', function(req,res){
+  res.render('index',{
+    'url' : req.protocol + '://' + req.get('host')
+  });
 });
 
 console.log('Starting server at port ' + (port || 8001) + '...');
